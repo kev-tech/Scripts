d27f28a813dc1d237150aff5d0fa9715027f3f3c
KC <kc@kevtech.co>
2020-02-09 13:12:12 -0500

    Rename check_hdd_price_new

--- check_hdd_price.py
+++ check_hdd_price.py
@@ -1,38 +1,26 @@
-from urllib.request import urlopen
-import re
-import subprocess
-import time
-import smtplib, ssl
+import requests
 from bs4 import BeautifulSoup
 
-port = 465
-sender = 'pythonicemails@gmail.com' #Change string to your desired send-from address.
-password = input("Enter your gmail password: ")
-recipient = 'mail@kevtech.co'
-message = ''
+page = requests.get("https://www.newegg.com/p/pl?d=western+digital+easystore").text
+soup = BeautifulSoup(page, "html.parser")
 
-url = 'https://www.newegg.com/p/2WA-0006-004A7?Description=western%20digital%20easy%20store&cm_re=western_digital_easy_store-_-9SIA8X5AJ26384-_-Product' #URL to crawl.
-page = urlopen(url).read()
-soup = BeautifulSoup(page, features="html.parser")
-result = ''
-price = ''
+title = soup.findAll("a", {"class": "item-title"})
+price = soup.findAll("li", {"class": "price-current"})
+shipping = soup.findAll("li", {"class": "price-ship"})
 
-def check_price():
-    price = soup.find('div', attrs={'div':'aside'})
-    message = 'price is: {}'.format(price)
-    print(message) #To delete.
 
-def send_email():
-    check_price()
-    # context = ssl.create_default_context()
-    # with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    #     server.login(sender, password) #Change email address to send from here.
-    #     server.sendmail(sender, recipient, message)
+# Outputs First Product
+title_print = print(title[0].text.strip())
+price_print = print(price[0].text.strip())
+shipping_print = print(shipping[0].text.strip())
 
-send_email()
 
-#TODO: 
-# - Incorrectly searching for aside/price-current HTML class.
-# - Add other urls to crawl. 
-# - Format email. 
-# - Test Comment
+# Outputs All Products
+for r in title:
+    print(r.text.strip())
+
+for r in price:
+    print(r.text.strip())
+    
+for r in shipping:
+    print(r.text.strip())
