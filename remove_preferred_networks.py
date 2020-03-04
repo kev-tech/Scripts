01a041942d3c1701c2518577b0a8ed71aa120ac8
kev <whiis@pm.me>
2020-03-04 15:29:22 -0500

    Fix typo in a comment in remove_preferred_networks.

--- remove_preferred_networks.py
+++ remove_preferred_networks.py
@@ -1,6 +1,6 @@
 #Written to remove specific preferred networks from student endpoints remotely via Mosyle MDM. 
 
-#If you need to remove more than one network, just copy/paste line 7 and change the interface and SSID name paramaters as needed.
+#If you need to remove more than one network, just copy/paste line 8 and change the interface and SSID name paramaters as needed.
 python << END
 import subprocess
 
